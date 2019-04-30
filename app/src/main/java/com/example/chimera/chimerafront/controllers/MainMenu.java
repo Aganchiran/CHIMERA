07040b4be5fa26263fafd9a9815be1bd0a1488af
Aganchiran <aganchiran@gmail.com>
2019-04-30 18:25:44 +0200

    T34 - Main menu & colors

---
+++ app/src/main/java/com/example/chimera/chimerafront/controllers/MainMenu.java
@@ -0,0 +1,15 @@
+package com.example.chimera.chimerafront.controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.chimera.R;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainmenu);
+    }
+
+}
