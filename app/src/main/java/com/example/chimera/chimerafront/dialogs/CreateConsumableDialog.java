787d132b9c4a7da6f7df7211ed5e9c96301dc73d
Aganchiran <aganchiran@gmail.com>
2019-05-08 11:45:23 +0200

    Modify & Delete character

---
+++ app/src/main/java/com/example/chimera/chimerafront/dialogs/CreateConsumableDialog.java
@@ -0,0 +1,6 @@
+package com.example.chimera.chimerafront.dialogs;
+
+import android.support.v7.app.AppCompatDialogFragment;
+
+public class CreateConsumableDialog extends AppCompatDialogFragment {
+}
