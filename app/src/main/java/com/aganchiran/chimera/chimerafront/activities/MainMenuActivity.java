af20dad0e8e7d7faadb11c312f2857cb5c62ca20
Aganchiran <aganchiran@gmail.com>
2019-06-04 09:48:59 +0200

    Orientation Main Menu

--- app/src/main/java/com/aganchiran/chimera/chimerafront/activities/MainMenuActivity.java
+++ app/src/main/java/com/aganchiran/chimera/chimerafront/activities/MainMenuActivity.java
@@ -1,8 +1,10 @@
 package com.aganchiran.chimera.chimerafront.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.aganchiran.chimera.R;
 
@@ -11,8 +13,29 @@ public class MainMenuActivity extends ActivityWithUpperBar {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         setContentView(R.layout.activity_main_menu);
+        int orientation = getResources().getConfiguration().orientation;
+        LinearLayout ol = findViewById(R.id.orientation_layout);
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            ol.setOrientation(LinearLayout.VERTICAL);
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ol.setOrientation(LinearLayout.HORIZONTAL);
+        }
 //        repopulateDatabase();
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        LinearLayout ol = findViewById(R.id.orientation_layout);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            ol.setOrientation(LinearLayout.VERTICAL);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ol.setOrientation(LinearLayout.HORIZONTAL);
+        }
     }
 
     public void goToCharactersList(final View view) {
