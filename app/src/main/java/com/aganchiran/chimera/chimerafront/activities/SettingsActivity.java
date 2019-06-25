e85234218562644033ce6694f832a037656b5774
Aganchiran <aganchiran@gmail.com>
2019-06-25 12:20:54 +0200

    Language WIP

--- app/src/main/java/com/aganchiran/chimera/chimerafront/activities/SettingsActivity.java
+++ app/src/main/java/com/aganchiran/chimera/chimerafront/activities/SettingsActivity.java
@@ -19,15 +19,44 @@
 package com.aganchiran.chimera.chimerafront.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 
 import com.aganchiran.chimera.R;
+import com.aganchiran.chimera.chimerafront.utils.LocaleHelper;
 
 public class SettingsActivity extends ActivityWithUpperBar {
+
+    public static final String ENGLISH = "en";
+    public static final String SPANISH = "es";
+
+    private RadioGroup languageRadioGroup;
+    private String languageCode = ENGLISH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_settings);
+
+        languageRadioGroup = findViewById(R.id.language_radio_group);
+
         super.onCreate(savedInstanceState);
     }
 
+    public void checklanguageButton(View view) {
+
+        switch (languageRadioGroup.getCheckedRadioButtonId()) {
+            case R.id.en:
+                languageCode = ENGLISH;
+                break;
+            case R.id.es:
+                languageCode = SPANISH;
+                break;
+            default:
+                languageCode = ENGLISH;
+        }
+
+        LocaleHelper.setLocale(this, languageCode);
+
+        recreate();
+    }
 }
